b51eaa845710302b82bac23f890c856dc3e8af23
Guboneui <starku2249@naver.com>
2021-11-16 05:31:54 +0900

    test

---
+++ Siwft Study/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Siwft Study
+//
+//  Created by 구본의 on 2021/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test")
+        
+    }
+
+}
