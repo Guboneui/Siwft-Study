1326ef5e98a7c2030ea0017b26104d7c3eba70b2
Guboneui <starku2249@naver.com>
2021-11-16 03:19:01 +0900

    Day_12 객체 생성자, 해제자 수정

--- Siwft Study/Day_12.playground/Contents.swift
+++ Siwft Study/Day_12.playground/Contents.swift
@@ -1,4 +1,9 @@
 import UIKit
+import Darwin
+
+
+// 클래스 -> 생성자를 만들어 주게 되면 메모리에 올라가게 됌.
+
 
 class Friend {
     var name = "본의"
@@ -58,3 +63,62 @@ if selfDestructingObject != nil {
 //weak = 약한 참조
 //weak 속성을 사용하는 객체는 항상 optional 타입이어야 한다.
 // 객체가 생성되지만 바로 객체가 해제되어 nil 이 된다 -> 메모리가 해제될 경우 자동으로 레퍼런스가 nil로 초기화 해준다.
+
+
+
+class K {
+    var name: String
+    
+    init(_ name: String = "이름 없음"){
+        self.name = name
+        print("K가 메모리에 올라갔습니다.")
+    }
+    
+    deinit{
+        print("K가 메모리에서 사라짐 - ", self.name)
+        
+    }
+    
+    var calledTimes = 0
+    let MaxTimes = 5
+    
+    static var instancesOfSelf = [K]()
+    class func destroySlef(object: K){
+        instancesOfSelf = instancesOfSelf.filter{(aK: K) in
+            aK !== object
+        }
+    }
+    
+    func call() {
+        calledTimes += 1
+        print("called \(calledTimes)")
+        if calledTimes > MaxTimes {
+            K.destroySlef(object: self)
+        }
+    }
+    
+    
+}
+
+let firstK = K("First_K")
+let secondK = K()
+
+// Unmanaged.passUnretained(객체).toopaque()
+// 로 해당 객체의 메모리 주소를 프린트 할 수 있다.
+
+let anObjectMemoryAddress = Unmanaged.passUnretained(firstK).toOpaque()
+let secondMemorryAddress = Unmanaged.passUnretained(secondK).toOpaque()
+
+print("firstK -> \(firstK)")
+print("anObjectMemoryAddress -> \(anObjectMemoryAddress)")
+
+print("secondK -> \(secondK)")
+print("secondMemorryAddress -> \(secondMemorryAddress)")
+
+
+weak var kDestoried = K("테스트")
+if kDestoried != nil {
+    kDestoried!.call()
+} else {
+    print("객체가 더이상 메모리에 없습니다.")
+}
