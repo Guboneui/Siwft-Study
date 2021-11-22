f0a1268abc16aa6c2eda65c208d8ec3116e4d6d9
Guboneui <starku2249@naver.com>
2021-11-23 01:09:21 +0900

    Swift zip()

---
+++ Siwft Study/Day_47.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let friends = ["영희", "철수", "존슨"]
+let pets = ["야옹이", "댕댕이", "찍찍이"]
+
+// 두 콜렉션을 zip을 이용해서 쌍으로 묶을 수 있다
+let zipped = zip(friends, pets)
+
+print(zipped)
+
+for item in zipped {
+    print("\(item.0) : \(item.1)")
+}
+
+let zippedArray = Array(zip(friends, pets))
+print(zippedArray)
+print(zippedArray[0])
