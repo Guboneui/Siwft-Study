2496eb0d76f5bb3071fb6835145ca8b691f94064
Guboneui <starku2249@naver.com>
2021-11-25 03:41:59 +0900

    Swift Toggle

---
+++ Siwft Study/Day_53.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var isIcedCoffee: Bool = false
+
+print("isIcedCoffee: \(isIcedCoffee)")
+
+// toggle을 통해 bool 값을 변경할 수 있다.
+isIcedCoffee.toggle()
+print("isIcedCoffee: \(isIcedCoffee)")
+
+isIcedCoffee.toggle()
+print("isIcedCoffee: \(isIcedCoffee)")
