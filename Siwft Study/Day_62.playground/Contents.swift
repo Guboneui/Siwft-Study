fa9fe4b9ac6958cf7e10b6fe09bbe003abcdec98
Guboneui <starku2249@naver.com>
2021-11-26 11:26:39 +0900

    Swift Set intersection

---
+++ Siwft Study/Day_62.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+// 배열
+let myFriends = ["철수", "영희", "수잔", "제시카"]
+
+let yourFriends = ["철수", "수잔", "제니퍼", "존시나"]
+
+// 배열을 set으로 만들기
+
+let myFriendsSet = Set(myFriends)
+let yourFriendsSet = Set(yourFriends)
+
+// 인터섹션으로 중복만 가져오기
+
+let commonFriends = myFriendsSet.intersection(yourFriendsSet)
+
+print(commonFriends)
