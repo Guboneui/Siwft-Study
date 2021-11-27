f0d7bcfee277da10efd97a3343b7236972876c51
Guboneui <starku2249@naver.com>
2021-11-28 00:44:41 +0900

    Swift 패턴 매칭 연산자 오버로드

---
+++ Siwft Study/Day_68.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// 패턴 매칭 연산자 오버로드
+// 문자 숫자가 일치 되는지 여부 추가!
+
+func ~= (pattern: String, value: Int) -> Bool {
+    return pattern == "\(value)"
+}
+
+// 패턴 ~= 값
+// 패턴 매칭 연산자 오버로드로
+// 입맛에 맞게 연산기능을 추가할 수 있습니다.
+
+if "20" ~= 20 {
+    print("글자와 숫자 일치!")
+    
+} else {
+    print("글자와 숫자 불일치")
+}
