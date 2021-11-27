12cfff87634bf74f252299496a57a0f45ec693c1
Guboneui <starku2249@naver.com>
2021-11-27 22:17:50 +0900

    Swift Any Type

---
+++ Siwft Study/Day_66.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// Any를 통해 어떠한 자료형도 받을 수 있다
+// 자료형이 Any인 배열
+
+var things = [Any]()
+
+struct Pet {
+    var name: String
+    var kind: String
+}
+
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("안녕하세요")
+things.append((3.0, 5.0))
+things.append(Pet(name: "댕댕이", kind: "강이지"))
+
+// 요롷게 클로저 함수도 넣을 수 있다
+
+things.append({(name: String) -> String in "안녕 친구들 \(name)"})
+
+let test = things[7]
+print(test)
+
+
+
