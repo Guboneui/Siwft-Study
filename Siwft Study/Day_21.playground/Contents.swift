3ad220efc1f4ae4d72c33c0c9ea9f728005658d4
Guboneui <starku2249@naver.com>
2021-12-03 03:11:15 +0900

    Swift protocol And protocol extension Edit

--- Siwft Study/Day_21.playground/Contents.swift
+++ Siwft Study/Day_21.playground/Contents.swift
@@ -8,9 +8,16 @@ import UIKit
 // 멤버변수, 메소드등을 꼭 구현하도록 강제함
 
 protocol Naming {
+    // name이란 변수를 가지고 있을것이라고 약속
     var name: String {get set}
+    
+    // getName이라는 메소드를 가지고 있을 것이라고 약속
     func getName() -> String
+    
+    func getNameIn(name: String) -> String
 }
+
+
 
 // Naming 프로토콜을 임플레먼트 했기 때문에
 
@@ -24,6 +31,10 @@ struct Friend: Naming {
     func getName() -> String {
         return "내 친구: " + self.name
     }
+    
+    func getNameIn(name: String) -> String {
+        return name + "입니다 " + self.name
+    }
 }
 
 let myFriend = Friend(name: "본의")
@@ -31,3 +42,15 @@ let name = myFriend.getName()
 
 print(myFriend)
 print(name)
+
+print(myFriend.getNameIn(name: "aaa"))
+
+
+// Day_57 프로토콜 익스텐션 참고
+// 프로토콜 익스텐션을 통해서 프로토콜을 상속받을 때 메소드를 옵셔널 처리 가능
+
+//extension Naming {
+//    func getNameIn(name: String) -> String {
+//        return name
+//    }
+//}
