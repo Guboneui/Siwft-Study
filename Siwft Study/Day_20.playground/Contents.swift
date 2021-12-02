9da70d81898362e75a59b9a3ae1fbe22c3cbec2a
Guboneui <starku2249@naver.com>
2021-12-03 01:45:30 +0900

    Swift Struct Method Edit

--- Siwft Study/Day_20.playground/Contents.swift
+++ Siwft Study/Day_20.playground/Contents.swift
@@ -14,3 +14,10 @@ var myFriend = Friend(lastName: "구", firstName: "본의")
 
 print(myFriend)
 print(myFriend.getFullName())
+
+// 스트럭트는 생성자를 만들지 않아도 자동으로 해준다.
+// var test = Friend(lastName: <#String#>, firstName: <#String#>)
+
+
+print(myFriend.lastName)
+print(myFriend.firstName)
